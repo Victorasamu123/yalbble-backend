126950548c702877d3210f8d9cd5abffb81529b5
Asamu victor <asamuvictor03@gmail.com>
2022-09-19 22:41:22 +0100

    completed signup controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,25 @@
 const authModel = require("../models/auth.model");
 
-const si
+const signup=(req,res)=>{
+    console.log(reg.body);
+    let form = authModel(req.body);
+    form.save((err)=>{
+        if(err){
+            console.log(err)
+            res.send({message:"signup not succesful",status:false})
+        }else{
+            authModel.findOne({email:req.body.email},(err,result)=>{
+                if(err){
+                    console.log(err)
+                    res.send({message:"signup not succesful",status:false})
+                }else{
+                    if(result.email){
+                        res.send({message:"email already exists",status:false})
+                    }else{
+                        res.send({message:"signup successfuul",status:true})
+                    }
+                }
+            })
+        }
+    })
+}
