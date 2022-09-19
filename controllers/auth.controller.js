feff4d840759211858a57d192407de20af492541
Asamu victor <asamuvictor03@gmail.com>
2022-09-19 23:06:31 +0100

    created email controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const authModel = require("../models/auth.model");
-
+const nodemailer = require('nodemailer');
+const PASSWORD= process.env.PASSWORD
 const signup=(req,res)=>{
     console.log(req.body);
     authModel.findOne({email:req.body.email},(err,result)=>{
@@ -24,6 +25,47 @@ const signup=(req,res)=>{
     })
     
 }
+const email =(req,res)=>{
+    console.log(req.body.email)
 
-module.exports={signup
+    const emailMessage = ` <center>
+    <div style="background-color:rgb(28,29,61); width: fit-content;height:fit-content;">
+      <h1 style="color: white;">welcome to myQuiz app</h1>
+      <div>
+        Dear ${req.body.lastname} ${req.body.firstname}
+      </div style="color: white;">
+      <div style="color: white;">
+        Welcome and thank you for<br>
+        registering! you can now start <br>
+        creating and rerecreating designs, we have differeents plans<br>
+        for you enjoy!!!💕💕💕👌
+      </div>
+    </div>
+</center>`
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'asamuvictor03@gmail.com',
+      pass: PASSWORD
+    }
+  });
+  
+  const mailOptions = {
+    from: 'asamuvictor03@gmail.com',
+    to: req.body.email,
+    subject: 'from the desk of the ceo',
+    // text: 'That was easy!'
+    html:emailMessage
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
+
+module.exports={signup,email
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const { signup } = require("../controllers/auth.controller");
+const { signup, email } = require("../controllers/auth.controller");
 const auth = express.Router()
 
 auth.post("/signup",signup)
+auth.post("/email",email)
 module.exports=auth;
